a5495d6e005e2f23faa8424e361d622ac475cf26
Macbook <akindele.o@klakpad.com>
2025-12-04 15:20:15 +0100

    feat: Started rewrite endpoint

--- app/main.py
+++ app/main.py
@@ -3,10 +3,12 @@ load_dotenv()
 
 from fastapi import FastAPI
 from app.api.routes_text import router as text_router
+from app.api.routes_rewrite import router as rewrite_router
 
 app = FastAPI(title="AI Agent")
 
 app.include_router(text_router, prefix="/ai-agent/v1", tags=["ai_agent"])
+app.include_router(rewrite_router, prefix="/ai-agent/v1", tags=["ai_agent"])
 
 @app.get("/")
 def read_root():
